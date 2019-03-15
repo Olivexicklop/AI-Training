819cf1ea856eedfe7a385cf8ae1d92cfd72d6f79
Olivexicklop <olivex@foxmail.com>
2019-03-15 15:01:38 +0800

    Screen Input

--- ScreenInput.py
+++ ScreenInput.py
@@ -1,20 +1,46 @@
 import numpy as np
 from PIL import ImageGrab
 import cv2
+import time
+from SimulateKeypress import PressKey, ReleaseKey, W, A, S, D
+# import pyautogui
+
+def RegionOfInterest(img, vertices):
+	mask = np.zeros_like(img)
+	cv2.fillPoly(mask, vertices, 255)
+	masked = cv2.bitwise_and(img, mask)
+	return masked
 
 
 def ProcessImg(OriginalImg):
 	ProcessedImg = cv2.cvtColor(OriginalImg, cv2.COLOR_BGR2GRAY)
 	ProcessedImg = cv2.Canny(ProcessedImg, threshold1=200, threshold2=300)
+	vertices = np.array([[10,500],[10,300],[300,200],[500,200],[800,300],[800,500]])
+	ProcessedImg = RegionOfInterest(ProcessedImg,[vertices])
 	return ProcessedImg
+
+
+for i in range(3,0,-1):
+	print(i)
+	time.sleep(1)
+	i -= 1
 
 
 
 while (True):
-	screen_img = ImageGrab.grab()
+	screen_img = np.array(ImageGrab.grab(bbox=(0,40,800,600)))
+	processed_screen_img = ProcessImg(screen_img)
 	# screen_numpy = np.array(screen_img.getdata(),dtype='uint8').reshape((screen_img.size[1],screen_img.size[0],3))
-	ImgToShow = cv2.cvtColor(np.array(screen_img), cv2.COLOR_BGR2RGB)
-	cv2.imshow('window',ImgToShow)
+	#ImgToShow = cv2.cvtColor(processed_screen_img, cv2.COLOR_BGR2RGB)
+	
+	# print("down")
+	# PressKey(W)
+	# time.sleep(3)
+	# ReleaseKey(W)
+	# print("up")
+
+	# cv2.resizeWindow("window",800,600)
+	cv2.imshow('window',processed_screen_img)
 	if cv2.waitKey(25) & 0xFF == ord('q'):
 		cv2.destroyAllWindows()
 		break
